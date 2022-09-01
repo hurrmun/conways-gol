749b54a965fc38f98f8a98b7bd5c85d7779a263e
hermanlyx <hermanlyx@gmail.com>
2022-09-01 23:08:02 +0800

    slight refactor

--- app.js
+++ app.js
@@ -17,21 +17,27 @@ const clickCell = (cell, row, column) => {
   console.log(board);
 };
 
-for (let i = 0; i < size; i++) {
-  const row = document.createElement('div');
-  row.classList.add('row');
+const createBoard = () => {
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
 
-  board.push([]);
-  displayBoard.appendChild(row);
-  const currentRow = document.querySelectorAll('.row')[i];
-  for (let j = 0; j < size; j++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.addEventListener('click', () => clickCell(cell, i, j));
+    board.push([]);
+    displayBoard.appendChild(row);
+    const currentRow = document.querySelectorAll('.row')[i];
+    for (let j = 0; j < size; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.addEventListener('click', () => clickCell(cell, i, j));
 
-    board[i].push(0);
-    currentRow.appendChild(cell);
+      board[i].push(0);
+      currentRow.appendChild(cell);
+    }
   }
-}
+};
 
-console.log(board);
+const startGame = () => {
+  createBoard();
+};
+
+startGame();
